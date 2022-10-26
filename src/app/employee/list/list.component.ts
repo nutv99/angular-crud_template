1423e76172c34daca800a34ade386b07d25aa6ec
nutv99 <nutv99@gmail.com>
2022-10-26 16:31:24 +0700

    3 files modified

--- src/app/employee/list/list.component.ts
+++ src/app/employee/list/list.component.ts
@@ -5,34 +5,38 @@ import { Emp } from '../../_models/emp';
 import { first } from 'rxjs';
 
 @Component({
-    selector: 'app-list',
-    templateUrl: './list.component.html',
-    styleUrls: ['./list.component.css']
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
+  employees!: Emp[];
+  totalrow: number = 0;
 
-    employees!: Emp[];
-    totalrow: number = 0;
+  headerTable = [ 'ชื่อ', 'นามสกุล', 'อีเมล์', 'เบอร์โทร',''];
 
-    constructor(private empService: EmpService,) { }
+  constructor(private empService: EmpService) {}
 
-    ngOnInit(): void {
+  ngOnInit(): void {
+    this.loadEmployee();
+  }
+
+  loadEmployee() {
+    this.empService
+      .getAll()
+      .pipe(first())
+      .subscribe((d) => {
+        this.employees = d;
+        this.totalrow = d.length;
+      });
+  }
+
+  delete(emp: Emp) {
+    this.empService
+      .delete(emp.empId)
+      .pipe(first())
+      .subscribe(() => {
         this.loadEmployee();
-    }
-
-    loadEmployee() {
-        this.empService.getAll().pipe(first())
-            .subscribe(d => {
-                this.employees = d;
-                this.totalrow = d.length;
-            });
-    }
-
-    delete(emp: Emp) {
-        this.empService.delete(emp.empId).pipe(first())
-            .subscribe(() => {
-                this.loadEmployee();
-            })
-    }
-
+      });
+  }
 }

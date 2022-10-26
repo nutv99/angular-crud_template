87498569ef38fb44634056f1be5341d5188cfe33
nutv99 <nutv99@gmail.com>
2022-10-26 16:50:19 +0700

    2 files modified, 6 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddeditComponent } from './employee/addedit/addedit.component';
 import { ListComponent } from './employee/list/list.component';
+import { DepartmentComponent } from './pages/department/department.component';
 
 const routes: Routes = [
-    { path: '', component: ListComponent },
-    { path: 'employee/add', component: AddeditComponent },
-    { path: 'employee/edit/:id', component: AddeditComponent },
+  { path: '', component: ListComponent },
+  { path: 'employee/add', component: AddeditComponent },
+  { path: 'employee/edit/:id', component: AddeditComponent },
+  { path: 'department', component: DepartmentComponent },
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
